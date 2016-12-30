ed4167c66f72e4f5ae8b5da683f1b140691ee246
Bruna Enos <bruna_bsm@icloud.com>
2016-12-30 17:53:01 -0600

    [fix] typo

--- db/config.js
+++ db/config.js
@@ -5,7 +5,7 @@
 const mysql = require("mysql")
 const Sequelize = require("sequelize")
 
-const db = new Sequelize(process.env.DATABASE_URL {
+const db = new Sequelize(process.env.DATABASE_URL, {
   // host: "localhost",
   dialect: "mysql",
   dialectOptions: {
