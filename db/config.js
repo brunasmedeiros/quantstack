b0a829cdec0e3f3476a57b84d869aa3f64c3ac93
Bruna Enos <bruna_bsm@icloud.com>
2016-12-30 17:41:09 -0600

    [test] heroku cleardb

--- db/config.js
+++ db/config.js
@@ -5,11 +5,11 @@
 const mysql = require("mysql")
 const Sequelize = require("sequelize")
 
-const db = new Sequelize(process.env.CLEARDB_DATABASE_URL || "quantstack", "root", "test", {
+const db = new Sequelize(process.env.DATABASE_URL || "quantstack", "root", "test", {
   // host: "localhost",
   dialect: "mysql",
   dialectOptions: {
-        ssl: true
+    ssl: true
   },
   define: {
     timestamps: false
